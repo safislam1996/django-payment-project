0c2006b1ce932353aa2af50214c5394d8ab223e8
safislam1996 <safi.096z@gmail.com>
2022-06-30 19:05:54 +0600

    phone number change and admin connection

--- payment/models.py
+++ payment/models.py
@@ -4,11 +4,13 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class Customer(models.Model):
     name=models.CharField(max_length=200)
-    phone_number=PhoneNumberField()
+    phone_number=PhoneNumberField(region='BD')
     balance=models.DecimalField(max_digits=6,decimal_places=2)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
 
-    def __init__(self) -> None:
-       return self.name
+    def __str__(self):
+               return self.name
+
+
